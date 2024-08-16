c9c97504bde7ac8703a91e2563eb417d2187ae38
Lynn <57570218+tangminhanh@users.noreply.github.com>
2024-08-17 02:51:35 +0800

    update page metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "Lynn Tang",
+  description: "Xin Chào! Welcome to my portfolio.",
 };
 
 import Hero from "@/components/hero-home";
